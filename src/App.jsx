40a0ef09ae8c0bebe1a0cabf78d263e3f766d0e1
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-11-15 17:04:24 -0500

    Firts Commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="h-screen grid place-items-center bg-black text-white">
+      <h1 className="text-4xl font-bold uppercase">Hello World</h1>
+    </div>
+  );
+};
